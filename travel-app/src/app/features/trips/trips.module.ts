5b44597917ed72b6fdc8e1eccbaead22dbdd23f5
miodraggosic <gosmio88@gmail.com>
2023-05-26 00:28:00 +0200

    update: Trips modul created

---
+++ travel-app/src/app/features/trips/trips.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TripsComponent } from './components/trips/trips.component';
+
+
+
+@NgModule({
+  declarations: [
+    TripsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TripsModule { }
