f5a12a468b02d5a9086f5ffc3d8860505e8dcd32
miodraggosic <gosmio88@gmail.com>
2023-05-26 00:23:48 +0200

    update: Title created

--- travel-app/src/app/shared/shared.module.ts
+++ travel-app/src/app/shared/shared.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavLinkComponent } from './components/nav-link/nav-link.component';
+import { TitleComponent } from './components/title/title.component';
 
-const COMPONENTS = [NavLinkComponent];
+const COMPONENTS = [NavLinkComponent, TitleComponent];
 
 @NgModule({
   declarations: [...COMPONENTS],
