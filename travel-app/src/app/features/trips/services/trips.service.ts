e757a1a27863db9848ac2008cb878eb52dbd7d1c
miodraggosic <gosmio88@gmail.com>
2023-05-26 03:08:30 +0200

    update: getTrips method added

--- travel-app/src/app/features/trips/services/trips.service.ts
+++ travel-app/src/app/features/trips/services/trips.service.ts
@@ -1,9 +1,18 @@
+import { Trip } from './../../../models/interfaces/trip.interface';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '@env';
+import { Observable, retry } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TripsService {
+  private readonly tripsUrl: string = `${environment.baseApiUrl}/trips`;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getTrips(): Observable<Trip[]> {
+    return this.http.get<Trip[]>(`${this.tripsUrl}`).pipe(retry(2));
+  }
 }
