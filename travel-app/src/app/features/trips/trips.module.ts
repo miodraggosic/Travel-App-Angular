06e7e16123e032a937a115970fa6eab846f28b59
miodraggosic <gosmio88@gmail.com>
2023-05-26 02:52:38 +0200

    update: TripsRouting created

--- travel-app/src/app/features/trips/trips.module.ts
+++ travel-app/src/app/features/trips/trips.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '@shared/shared.module';
+
+import { TripsRoutingModule } from './trips-routing.module';
+
+import { MatIconModule } from '@angular/material/icon';
+
+import { HomepageComponent } from './components/homepage/homepage.component';
 import { TripsComponent } from './components/trips/trips.component';
 
-
-
 @NgModule({
-  declarations: [
-    TripsComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [TripsComponent, HomepageComponent],
+  imports: [CommonModule, SharedModule, MatIconModule, TripsRoutingModule],
 })
-export class TripsModule { }
+export class TripsModule {}
