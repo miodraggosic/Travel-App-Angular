c6eaf6b8a0b18d08edac58f754d7b1a9580584a6
miodraggosic <gosmio88@gmail.com>
2023-05-26 03:07:11 +0200

    update: RouterModule & HomeLayout added

--- travel-app/src/app/core/core.module.ts
+++ travel-app/src/app/core/core.module.ts
@@ -1,19 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeLayoutComponent } from './layouts/home-layout/home-layout.component';
 
-const COMPONENTS = [HeaderComponent, FooterComponent];
+const COMPONENTS = [HeaderComponent, FooterComponent, HomeLayoutComponent];
 
 @NgModule({
   declarations: [...COMPONENTS],
-  imports: [CommonModule, SharedModule, MatToolbarModule, MatListModule],
+  imports: [
+    CommonModule,
+    SharedModule,
+    RouterModule,
+    MatToolbarModule,
+    MatListModule,
+  ],
   exports: [...COMPONENTS],
 })
 export class CoreModule {}
