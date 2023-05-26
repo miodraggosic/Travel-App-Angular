2e6a29199252b8a67e11cbd5c94319429c9f0e14
miodraggosic <gosmio88@gmail.com>
2023-05-26 03:31:29 +0200

    update: HttpClientModule added

--- travel-app/src/app/app.module.ts
+++ travel-app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     AppRoutingModule,
     CoreModule,
   ],
