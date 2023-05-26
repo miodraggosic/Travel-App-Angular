f830eb7e141844600670773205cae62f7dc7657c
miodraggosic <gosmio88@gmail.com>
2023-05-26 03:09:51 +0200

    update: getTrips logic added

--- travel-app/src/app/features/trips/components/trips/trips.component.ts
+++ travel-app/src/app/features/trips/components/trips/trips.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { TripsService } from '../../services/trips.service';
+import { take } from 'rxjs';
+import { Trip } from 'src/app/models/interfaces/trip.interface';
 
 @Component({
   selector: 'app-trips',
   templateUrl: './trips.component.html',
-  styleUrls: ['./trips.component.scss']
+  styleUrls: ['./trips.component.scss'],
 })
 export class TripsComponent implements OnInit {
+  public trips: Trip[] = [];
 
-  constructor() { }
+  constructor(private tripsService: TripsService) {}
 
   ngOnInit(): void {
+    this.getTrips(), console.log(this.trips);
   }
 
+  getTrips(): void {
+    this.tripsService
+      .getTrips()
+      .pipe(take(1))
+      .subscribe((data) => (this.trips = data));
+  }
 }
