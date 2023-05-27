4d527ee20bc9ffe56844d9af88a007753c2d2cfe
miodraggosic <gosmio88@gmail.com>
2023-05-27 18:28:22 +0200

    update: Search created

--- travel-app/src/app/shared/shared.module.ts
+++ travel-app/src/app/shared/shared.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 import { NavLinkComponent } from './components/nav-link/nav-link.component';
 import { TitleComponent } from './components/title/title.component';
+import { SearchComponent } from './components/search/search.component';
 
-const COMPONENTS = [NavLinkComponent, TitleComponent];
+const COMPONENTS = [NavLinkComponent, TitleComponent, SearchComponent];
 
 @NgModule({
   declarations: [...COMPONENTS],
-  imports: [CommonModule],
+  imports: [CommonModule, MatFormFieldModule, MatInputModule],
   exports: [...COMPONENTS],
 })
 export class SharedModule {}
