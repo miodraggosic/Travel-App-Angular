7d09d2efa911c75e8cd579f2e60ee63dd89bc126
miodraggosic <gosmio88@gmail.com>
2023-05-27 16:01:28 +0200

    update: routing to trips added

--- travel-app/src/app/features/trips/trips-routing.module.ts
+++ travel-app/src/app/features/trips/trips-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { TripsComponent } from './components/trips/trips.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomepageComponent,
+  },
+  {
+    path: 'trips',
+    component: TripsComponent,
   },
 ];
 
