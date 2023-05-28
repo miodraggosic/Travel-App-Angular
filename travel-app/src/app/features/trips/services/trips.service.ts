1296059f77204766dd08c2c41383ab1f85841980
miodraggosic <gosmio88@gmail.com>
2023-05-28 17:58:09 +0200

    update: getById & getFiltered methods  added

--- travel-app/src/app/features/trips/services/trips.service.ts
+++ travel-app/src/app/features/trips/services/trips.service.ts
@@ -2,7 +2,7 @@ import { Trip } from './../../../models/interfaces/trip.interface';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@env';
-import { Observable, retry } from 'rxjs';
+import { Observable, catchError, of, retry } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -14,5 +14,14 @@ export class TripsService {
 
   getTrips(): Observable<Trip[]> {
     return this.http.get<Trip[]>(`${this.tripsUrl}`).pipe(retry(2));
+  }
+
+  getById(id: number): Observable<Trip> {
+    return this.http
+      .get<Trip>(`${this.tripsUrl}/${id}`)
+      .pipe(catchError((err) => of(err)));
+  }
+  getFiltered(term: string): Observable<Trip[]> {
+    return this.http.get<Trip[]>(`${this.tripsUrl}?title_like=${term}`);
   }
 }
