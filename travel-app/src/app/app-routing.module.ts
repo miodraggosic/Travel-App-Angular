fde1c9b14e2ac6270ba345b0481d699b444f53ea
miodraggosic <gosmio88@gmail.com>
2023-05-30 08:25:42 +0200

    update: routes & guards added

--- travel-app/src/app/app-routing.module.ts
+++ travel-app/src/app/app-routing.module.ts
@@ -3,16 +3,36 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeLayoutComponent } from './core/layouts/home-layout/home-layout.component';
 import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component';
+import { UserLayoutComponent } from './core/layouts/user-layout/user-layout.component';
+import { LoginComponent } from './core/auth/components/login/login.component';
+import { RegisterComponent } from './core/auth/components/register/register.component';
+import { AuthGuard } from './core/auth/guards/auth.guard';
+import { AdminGuard } from './core/auth/guards/admin.guard';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    component: UserLayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'register',
+        pathMatch: 'full',
+      },
+      {
+        path: 'register',
+        component: RegisterComponent,
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+    ],
   },
   {
     path: 'home',
     component: HomeLayoutComponent,
+    canActivate: [AuthGuard],
     children: [
       {
         path: '',
@@ -21,6 +41,7 @@ const routes: Routes = [
       },
       {
         path: 'admin',
+        canLoad: [AdminGuard],
         loadChildren: () =>
           import('./features/admin/admin.module').then((m) => m.AdminModule),
       },
--- travel-app/src/app/features/admin/admin-routing.module.ts
+++ travel-app/src/app/features/admin/admin-routing.module.ts
@@ -3,14 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AdminTableComponent } from './components/admin-table/admin-table.component';
 import { CreateTripComponent } from './components/create-trip/create-trip.component';
+import { AdminGuard } from 'src/app/core/auth/guards/admin.guard';
 
 const routes: Routes = [
   {
     path: '',
+    canActivate: [AdminGuard],
     component: AdminTableComponent,
   },
   {
     path: 'create',
+    canActivate: [AdminGuard],
     component: CreateTripComponent,
   },
 ];
