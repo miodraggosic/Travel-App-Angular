bb5e0fa5ae216808f1d4482ad5261e21311ec229
miodraggosic <gosmio88@gmail.com>
2023-05-30 05:39:49 +0200

    update: Expansion panel added

--- travel-app/src/app/features/trips/trips.module.ts
+++ travel-app/src/app/features/trips/trips.module.ts
@@ -6,6 +6,7 @@ import { TripsRoutingModule } from './trips-routing.module';
 
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { TripsComponent } from './components/trips/trips.component';
@@ -14,12 +15,19 @@ import { TripCardComponent } from './components/trip-card/trip-card.component';
 import { TripOverviewComponent } from './components/trip-overview/trip-overview.component';
 
 @NgModule({
-  declarations: [TripsComponent, HomepageComponent, TripComponent, TripCardComponent, TripOverviewComponent],
+  declarations: [
+    TripsComponent,
+    HomepageComponent,
+    TripComponent,
+    TripCardComponent,
+    TripOverviewComponent,
+  ],
   imports: [
     CommonModule,
     SharedModule,
     MatIconModule,
     MatCardModule,
+    MatExpansionModule,
     TripsRoutingModule,
   ],
 })
