26b853df96046512d1208b9083e59a7b4fbe0ab0
miodraggosic <gosmio88@gmail.com>
2023-05-30 08:24:04 +0200

    update: AuthGuard created

---
+++ travel-app/src/app/core/auth/guards/auth.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, CanLoad, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate, CanLoad {
+  constructor(private router: Router, private authService: AuthService) {}
+
+  canLoad(): boolean {
+    return this.isAuthentificated();
+  }
+
+  canActivate(): boolean {
+    return this.isAuthentificated();
+  }
+
+  private isAuthentificated(): boolean {
+    if (!this.authService.isAuthentificated()) {
+      console.log('auth', this.authService.isAuthentificated());
+
+      this.router.navigateByUrl('/login');
+      return false;
+    }
+    return true;
+  }
+}
